bba17198eefe8dbe20ccb875400dba43d47d0deb
JyuKa <yoyksks3@gmail.com>
2021-08-03 21:17:40 +0900

    change class

--- src/main/java/com/fastcampus/jpa/bookmanager/domain/Sample.java
+++
@@ -1,6 +0,0 @@
-package com.fastcampus.jpa.bookmanager.domain;
-
-public class Sample {
-
-    // test class
-}
---
+++ src/main/java/com/fastcampus/jpa/bookmanager/domain/listener/Sample.java
@@ -0,0 +1,6 @@
+package com.fastcampus.jpa.bookmanager.domain.listener;
+
+public class Sample {
+
+    // test class
+}
