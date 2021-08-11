01b2cef3e79aa5d4379f2979933403080f4bd6ff
JyuKa <yoyksks3@gmail.com>
2021-08-11 17:52:47 +0900

    Transaction isolation & propagation Temp

--- src/main/java/com/fastcampus/jpa/bookmanager/service/BookService.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/service/BookService.java
@@ -7,7 +7,11 @@ import com.fastcampus.jpa.bookmanager.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @Service
 @RequiredArgsConstructor
@@ -15,25 +19,55 @@ public class BookService {
 
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
+    private final EntityManager entityManager;
+    private final AuthorService authorService;
 
-    @Transactional
-    public void putBookAndAuthor() throws Exception {
+//    public void put(){
+//        this.putBookAndAuthor();
+//    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void putBookAndAuthor() {
         Book book = new Book();
         book.setName("JPA 시작하기");
 
         bookRepository.save(book);
 
-        Author author = new Author();
-        author.setName("martin");
+        try{
+            authorService.putAuthor();
+        }catch(RuntimeException e){
+        }
 
-        authorRepository.save(author);
+//        Author author = new Author();
+//        author.setName("martin");
+//
+//        authorRepository.save(author);
+//
+//        // RuntimeExceptino : commit이 일어나지 않음
+////        throw new RuntimeException("오류가 나서 DB commit이 발생하지 않습니다.");
+//
+//        // 선택적 Exception : 예외가 일어나도 commit은 일어남
+        throw new RuntimeException("BookService에서 RuntimeException 발생");
 
-        // RuntimeExceptino : commit이 일어나지 않음
-//        throw new RuntimeException("오류가 나서 DB commit이 발생하지 않습니다.");
+    }
 
-        // 선택적 Exception : 예외가 일어나도 commit은 일어남
-        throw new Exception("abc");
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public void get(Long id){
+        System.out.println(">>> " + bookRepository.findById(id));
+        System.out.println(">>> " + bookRepository.findAll());
 
+        entityManager.clear();
+
+        System.out.println(">>> " + bookRepository.findById(id));
+        System.out.println(">>> " + bookRepository.findAll());
+
+        bookRepository.update();
+        entityManager.clear();
+
+
+//        Book book = bookRepository.findById(id).get();
+//        book.setName("바뀔까?");
+//        bookRepository.save(book);
     }
 
 }
